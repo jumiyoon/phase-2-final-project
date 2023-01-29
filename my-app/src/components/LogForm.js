6cc56b7ab4f887fccdf112c8a6160ad24a0e592d
Jumi Yoon <jums.code@gmail.com>
2023-01-28 17:18:02 -0800

    add form submit event listener

--- my-app/src/components/LogForm.js
+++ my-app/src/components/LogForm.js
@@ -3,6 +3,18 @@ import { Button, Form, Container, Segment } from "semantic-ui-react";
 
 
 function LogForm( { mode }){
+    function handleSubmit(event) {
+        const newLogData = {
+            title: event.target[0].value,
+            author: event.target[1].value,
+            url: event.target[2].value,
+            genre: event.target[3].value,
+            category:event.target[4].value,
+            thoughts: event.target[5].value
+        }
+
+        onSubmit(newLogData)
+    }
 
     const genres = [
         { key: "f", text: "Fiction", value: 'fiction' },
@@ -10,15 +22,13 @@ function LogForm( { mode }){
         { key: "pr", text: "Philosophy / Religion", value: "philRel"},
         { key: "be", text: "Business / Economics", value: "bizEcon"}
       ]
-      
-
 
     return (
         <div>
             {mode ? <Container>
                     <Segment inverted>
                     <h1>Create New Log</h1>
-                    <Form inverted>
+                    <Form inverted onSubmit={handleSubmit}>
                         <Form.Group >
                             <Form.Input fluid label="Title" placeholder="Title" width={5}/>
                             <Form.Input fluid label="Author" placeholder="Author" width={5} />
@@ -51,7 +61,7 @@ function LogForm( { mode }){
                 </Container> : 
                 <Container>
                 <h1>Create New Log</h1>
-                <Form >
+                <Form onSubmit={handleSubmit}>
                     <Form.Group >
                         <Form.Input fluid label="Title" placeholder="Title" width={5}/>
                         <Form.Input fluid label="Author" placeholder="Author" width={5} />
