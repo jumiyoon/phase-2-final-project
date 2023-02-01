fde7492ccdc9aad9c0481016a82bcd55a2657357
Jumi Yoon <jums.code@gmail.com>
2023-01-31 17:54:29 -0800

    change form to controlled components

--- my-app/src/components/LogForm.js
+++ my-app/src/components/LogForm.js
@@ -3,18 +3,28 @@ import { Button, Form, Container, Segment } from "semantic-ui-react";
 
 
 function LogForm( { mode, onNewLogSubmit }){
+    const [ form, setForm ] = useState({
+        title: "The Old Man and the Sea",
+        author: "Ernest Hemingway",
+        genre: "Fiction",
+        category: "Book",
+        url: "www.google.com",
+        thoughts: "This was interesting..."
+    })
+
+    function handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setForm({
+            ...form,
+            [name]: value});
+    }
+
     function handleSubmit(event) {
-        console.log(event)
-        const newLogData = {
-            "title": event.target[0].value,
-            "author": event.target[1].value,
-            "url": event.target[2].value,
-            "genre": genre,
-            "category": category,
-            "thoughts": event.target[5].value
-        }
-        console.log(newLogData);
-        onNewLogSubmit(newLogData);
+        console.log(form);
+        onNewLogSubmit(form);
+
 
     }
 
@@ -24,18 +34,6 @@ function LogForm( { mode, onNewLogSubmit }){
         { key: "pr", text: "Philosophy / Religion", value: "Philosophy / Religion"},
         { key: "be", text: "Business / Economics", value: "Business / Economics"}
     ]
-
-    const [ genre, setGenre ] = useState("")
-    function handleGenre(e) {
-        setGenre(e.target.textContent)
-    }
-
-    const [ category, setCategory ] = useState("")
-     function handleCategory(category) {
-        setCategory(category)
-    }
-
-
     return (
         <div>
             {mode ? <Container>
@@ -43,16 +41,17 @@ function LogForm( { mode, onNewLogSubmit }){
                     <h1>Create New Log</h1>
                     <Form size="large" inverted onSubmit={handleSubmit}>
                         <Form.Group >
-                            <Form.Input fluid label="Title" placeholder="Title" width={5}/>
-                            <Form.Input fluid label="Author" placeholder="Author" width={5} />
-                            <Form.Input fluid label="URL" placeholder="URL" width={10} />
+                            <Form.Input fluid label="Title" placeholder="Title" name="title" width={5} onChange={handleChange} />
+                            <Form.Input fluid label="Author" placeholder="Author" name="author" width={5} onChange={handleChange} />
+                            <Form.Input fluid label="URL" placeholder="URL" name="url" width={10} onChange={handleChange} />
                             <Form.Select
                                 fluid
                                 label="Genre"
+                                name="genre"
                                 options={genres}
                                 placeholder='Genre'
                                 width={7}
-                                onChange={handleGenre}
+                                onChange={handleChange}
                             />
                         </Form.Group>
                         <Form.Group inline>
@@ -61,19 +60,21 @@ function LogForm( { mode, onNewLogSubmit }){
                             label='Book'
                             control='input'
                             type='radio'
-                            name='Book'
-                            onChange={()=>handleCategory("Book")}
+                            name='category'
+                            value="Book"
+                            onChange={handleChange}
                         />
                         <Form.Field
                             label='TedTalk / Podcast'
                             control='input'
                             type='radio'
-                            name='Book'
-                            onChange={()=>handleCategory("TedTalk / Podcast")}
+                            name='category'
+                            value="TedTalk / Podcast"
+                            onChange={handleChange}
                         />
                         </Form.Group>
                         <Form.Group>
-                            <Form.Field width={15} label="Thoughts" placeholder="My Thoughts" control='textarea' rows='5' />
+                            <Form.Field width={15} label="Thoughts" placeholder="My Thoughts" control='textarea' rows='5' onChange={handleChange} />
                         </Form.Group>
                         <Button type='submit'>Submit</Button>
                     </Form>
@@ -81,42 +82,45 @@ function LogForm( { mode, onNewLogSubmit }){
                 </Container> : 
                 <Container>
                 <h1>Create New Log</h1>
-                <Form size="large" onSubmit={handleSubmit}>
-                    <Form.Group >
-                        <Form.Input fluid label="Title" placeholder="Title" width={5}/>
-                        <Form.Input fluid label="Author" placeholder="Author" width={5} />
-                        <Form.Input fluid label="URL" placeholder="URL" width={10} />
-                        <Form.Select
-                            fluid
-                            label="Genre"
-                            options={genres}
-                            placeholder='Genre'
-                            width={7}
-                            onChange={handleGenre}
+                    <Form size="large" onSubmit={handleSubmit}>
+                        <Form.Group >
+                            <Form.Input fluid label="Title" placeholder="Title" name="title" width={5} onChange={handleChange} />
+                            <Form.Input fluid label="Author" placeholder="Author" name="author" width={5} onChange={handleChange} />
+                            <Form.Input fluid label="URL" placeholder="URL" name="url" width={10} onChange={handleChange} />
+                            <Form.Select
+                                fluid
+                                label="Genre"
+                                name="genre"
+                                options={genres}
+                                placeholder='Genre'
+                                width={7}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+                        <Form.Group inline>
+                        <label>Category</label>
+                        <Form.Field
+                            label='Book'
+                            control='input'
+                            type='radio'
+                            name='category'
+                            value="Book"
+                            onChange={handleChange}
                         />
-                    </Form.Group>
-                    <Form.Group inline>
-                    <label>Category</label>
-                    <Form.Field
-                        label='Book'
-                        control='input'
-                        type='radio'
-                        name='Book'
-                        onChange={()=>handleCategory("Book")}
-                    />
-                    <Form.Field
-                        label='TedTalk / Podcast'
-                        control='input'
-                        type='radio'
-                        name='Book'
-                        onChange={()=>handleCategory("TedTalk / Podcast")}
-                    />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Field width={15} label="Thoughts" placeholder="My Thoughts" control='textarea' rows='5' />
-                    </Form.Group>
-                    <Button type='submit'>Submit</Button>
-                </Form>
+                        <Form.Field
+                            label='TedTalk / Podcast'
+                            control='input'
+                            type='radio'
+                            name='category'
+                            value="TedTalk / Podcast"
+                            onChange={handleChange}
+                        />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Field width={15} name="thoughts" label="Thoughts" placeholder="My Thoughts" control='textarea' rows='5' onChange={handleChange} />
+                        </Form.Group>
+                        <Button type='submit'>Submit</Button>
+                    </Form>
             </Container>}
         </div>
     )
