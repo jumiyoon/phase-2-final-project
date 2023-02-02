3863fca047aeeddab73cbf1a3fb9caebcc6d27cb
Jumi Yoon <jums.code@gmail.com>
2023-02-02 14:10:09 -0800

    add LogCardDetail component

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -7,6 +7,7 @@ import darkImage from "/Users/jumiyoon/Development/code/phase-2/phase-2-final-pr
 import LogsPage from "./LogsPage";
 import NavBar from "./NavBar";
 import LogForm from "./LogForm";
+import LogCardDetail from "./LogCardDetail"''
 
 
 
@@ -88,6 +89,9 @@ function App() {
               filterByTitle={filterByTitle}
               searchBy={searchBy} />
           </Route>
+            <Route path="/:id">
+              <LogCardDetail />
+            </Route>
           <Route path="/newlog">
             <LogForm mode={isDarkMode} onFormSubmit={onFormSubmit} postRequest={addLog} />
           </Route>
