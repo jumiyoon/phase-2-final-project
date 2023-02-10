d3822b14387e01a850fbad568c551a6006944d93
Jumi Yoon <jums.code@gmail.com>
2023-02-10 13:29:35 -0800

    Updates

---
+++ precache-manifest.b2e0d2459a5d9c9255637ce6fe471da1.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "c84e0bded0b500ed45b7",
+    "url": "/phase-2-final-project/static/css/main.9ef22009.chunk.css"
+  },
+  {
+    "revision": "c84e0bded0b500ed45b7",
+    "url": "/phase-2-final-project/static/js/main.9660ee5c.chunk.js"
+  },
+  {
+    "revision": "d6150f1eef8f2cbe2a90",
+    "url": "/phase-2-final-project/static/js/runtime~main.0e08163c.js"
+  },
+  {
+    "revision": "6484ecf603c2fbcb3f25",
+    "url": "/phase-2-final-project/static/css/2.717b8dc0.chunk.css"
+  },
+  {
+    "revision": "6484ecf603c2fbcb3f25",
+    "url": "/phase-2-final-project/static/js/2.c0aba562.chunk.js"
+  },
+  {
+    "revision": "8ecfe641118e170f1292d2ef058c701f",
+    "url": "/phase-2-final-project/static/media/updated book.8ecfe641.jpg"
+  },
+  {
+    "revision": "6651dbf0adcff5b3861f6f58ec627a3d",
+    "url": "/phase-2-final-project/static/media/updated podcast.6651dbf0.jpg"
+  },
+  {
+    "revision": "bf8a25c821e7124e6e3e6762d0f4e558",
+    "url": "/phase-2-final-project/static/media/square-background.bf8a25c8.png"
+  },
+  {
+    "revision": "1fe36f1f67fc61ffc76bb1ef99fbacd7",
+    "url": "/phase-2-final-project/static/media/background-darkmode.1fe36f1f.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/phase-2-final-project/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/phase-2-final-project/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/phase-2-final-project/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/phase-2-final-project/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/phase-2-final-project/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/phase-2-final-project/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/phase-2-final-project/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/phase-2-final-project/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/phase-2-final-project/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/phase-2-final-project/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/phase-2-final-project/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/phase-2-final-project/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/phase-2-final-project/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/phase-2-final-project/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/phase-2-final-project/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/phase-2-final-project/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "5fe94fef85831aef18c77e7d007b369f",
+    "url": "/phase-2-final-project/index.html"
+  }
+];
